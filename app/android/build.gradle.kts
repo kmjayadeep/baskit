c35b7889a4bd7c203eddff360a7c48120ca0a6f1
Jayadeep KM <kmjayadeep@gmail.com>
2025-06-21 09:52:55 +0200

    android build fix
    Signed-off-by: Jayadeep KM <kmjayadeep@gmail.com>

--- app/android/build.gradle.kts
+++ app/android/build.gradle.kts
@@ -1,10 +1,3 @@
-buildscript {
-    dependencies {
-        classpath("com.android.tools.build:gradle:8.1.0")
-        classpath("com.google.gms:google-services:4.4.0")
-    }
-}
-
 allprojects {
     repositories {
         google()
