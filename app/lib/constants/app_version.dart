debf3fd9442823b8fc40c5a5c9f5d9a8857df889
Jayadeep KM <kmjayadeep@gmail.com>
2025-10-15 07:01:08 +0200

    chore: bump version to 4.12.0+80

--- app/lib/constants/app_version.dart
+++ app/lib/constants/app_version.dart
@@ -2,5 +2,5 @@
 /// This file is automatically updated by the release script
 class AppVersion {
   /// Current app version (semantic version)
-  static const String version = '4.11.8';
+  static const String version = '4.12.0';
 }
